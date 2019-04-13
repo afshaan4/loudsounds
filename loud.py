bf84ca35a1c577f7f7949b90af7cadc37d87e7b9
cautiouspotato <afshaanlondhe4@gmail.com>
2019-04-13 12:28:34 +0530

    checkpoint befor i makew a mess

--- loud.py
+++ loud.py
@@ -3,11 +3,11 @@
 # stolen from here: https://stackoverflow.com/questions/4160175/detect-tap-with-pyaudio-from-live-mic
 # modified to listen for loud stuff in general instead of just taps
 
-import audiorecorder
 import datetime
 import argparse
 import pyaudio
 import struct
+import wave
 import math
 
 # I know this is messy, some global vars here, some in the class
@@ -78,8 +78,8 @@ def get_rms(block):
 class loudTester(object):
     def __init__(self):
         self.pa = pyaudio.PyAudio()
-        self.stream = self.start_recording()
-        self.rec = audiorecorder.Recorder(channels = 1)
+        self.stream = self.mic_stream()
+        self.rec = Recorder(channels = 1)
         self.recfile = self.rec.open(args.filename, 'wb')
         self.tap_threshold = args.sensitivity
         self.noisycount = args.noise_length+1 
@@ -107,7 +107,8 @@ class loudTester(object):
 
         return device_index
 
-    def start_recording(self):
+    # stream for detecting noise
+    def mic_stream(self):
         device_index = self.find_input_device()
 
         stream = self.pa.open(  format = FORMAT,
@@ -120,10 +121,10 @@ class loudTester(object):
 
     def soundDetected(self):
         print("YEET!++++++++++++++++++")
-        #self.recfile.start_recording()
+        self.recfile.start_recording()
 
     def soundEnded(self):
-        #self.recfile.stop_recording()
+        self.recfile.stop_recording()
         print("NO U------------------")
 
 
@@ -160,6 +161,98 @@ class loudTester(object):
             if self.quietcount > UNDERSENSITIVE:
                 # turn up the sensitivity
                 self.tap_threshold *= 0.9
+
+
+class Recorder(object):
+    '''A recorder class for recording audio to a WAV file.
+    Records in mono by default.
+    '''
+
+    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024):
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+
+    def open(self, fname, mode='wb'):
+        return RecordingFile(fname, mode, self.channels, self.rate,
+                            self.frames_per_buffer)
+
+class RecordingFile(object):
+    def __init__(self, fname, mode, channels, 
+                rate, frames_per_buffer):
+        self.fname = fname
+        self.mode = mode
+        self.channels = channels
+        self.rate = rate
+        self.frames_per_buffer = frames_per_buffer
+        self._pa = pyaudio.PyAudio()
+        self.wavefile = self._prepare_file(self.fname, self.mode)
+        self._stream = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception, value, traceback):
+        self.close()
+
+
+    # finds the mic
+    def find_input_device(self):
+        device_index = None
+        for i in range( self._pa.get_device_count() ):     
+            devinfo = self._pa.get_device_info_by_index(i)   
+            print( "Device %d: %s"%(i,devinfo["name"]) )
+
+            # this stuff might change so if you get stupid
+            # device not found issues, add a word from
+            # your devices name in here(in lower case)
+            for keyword in ["mic", "input", "usb"]:
+                if keyword in devinfo["name"].lower():
+                    print("Found an input: device %d - %s"%(i,devinfo["name"]))
+                    device_index = i
+                    return device_index
+
+        if device_index == None:
+            print( "No preferred input found; using default input device." )
+
+        return device_index
+
+    def start_recording(self):
+        device_index = self.find_input_device()
+
+        # Use a stream with a callback in non-blocking mode
+        self._stream = self._pa.open(format=pyaudio.paInt16,
+                                        channels=self.channels,
+                                        rate=self.rate,
+                                        input=True,
+                                        input_device_index = device_index,
+                                        frames_per_buffer=self.frames_per_buffer,
+                                        stream_callback=self.get_callback())
+        self._stream.start_stream()
+        return self
+
+    def stop_recording(self):
+        self._stream.stop_stream()
+        return self
+
+    def get_callback(self):
+        def callback(in_data, frame_count, time_info, status):
+            self.wavefile.writeframes(in_data)
+            return in_data, pyaudio.paContinue
+        return callback
+
+
+    def close(self):
+        self._stream.close()
+        self._pa.terminate()
+        self.wavefile.close()
+
+    def _prepare_file(self, fname, mode='wb'):
+        wavefile = wave.open(fname, mode)
+        wavefile.setnchannels(self.channels)
+        wavefile.setsampwidth(self._pa.get_sample_size(pyaudio.paInt16))
+        wavefile.setframerate(self.rate)
+        return wavefile
 
 
 if __name__ == "__main__":
